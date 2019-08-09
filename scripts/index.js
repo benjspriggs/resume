e7871ba83dc234e68eacb309c55362bdc897c4b4
Benjamin Spriggs <ben@sprico.com>
2019-08-09 16:26:52 -0700

    Unobfuscate via JS

--- scripts/index.js
+++ scripts/index.js
@@ -58,3 +58,17 @@
         document.documentElement.classList.add("js");
     }
 }());
+
+(function () {
+    window.onload = function () {
+        const links = document.getElementsByClassName("email-me")
+
+        Array.from(links).forEach(function (element) {
+            const name = element.getAttribute("data-address-name"),
+                domain = element.getAttribute("data-address-domain"),
+                tld = element.getAttribute("data-address-tld");
+
+            element.href = "mailto:" + name + "@" + domain + "." + tld;
+        });
+    }
+}());
