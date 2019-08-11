ee6dd5f228b71de4dfb60029ea08cbea53198c9d
bspriggs <ben@sprico.com>
2019-08-11 14:02:48 -0700

    Indent

--- scripts/index.js
+++ scripts/index.js
@@ -1,120 +1,120 @@
 (function () {
-    window.addEventListener("load", function () {
-        document.documentElement.classList.remove("no-js");
-        document.documentElement.classList.add("js");
-    });
+  window.addEventListener("load", function () {
+    document.documentElement.classList.remove("no-js");
+    document.documentElement.classList.add("js");
+  });
 }());
 
 (function () {
-    let lastPageY = 0;
-    let direction = "down";
-    let shouldSetVisibilityFromScroll = true;
-    const mainMenu = document.getElementById("main-menu");
-    const mainMenuPhantom = mainMenu.cloneNode(true);
-    mainMenuPhantom.id = "";
-    mainMenuPhantom.classList.add("phantom");
+  let lastPageY = 0;
+  let direction = "down";
+  let shouldSetVisibilityFromScroll = true;
+  const mainMenu = document.getElementById("main-menu");
+  const mainMenuPhantom = mainMenu.cloneNode(true);
+  mainMenuPhantom.id = "";
+  mainMenuPhantom.classList.add("phantom");
 
-    const scrollDirection = {
-        'DOWN': 'down',
-        'UP': 'up'
-    };
+  const scrollDirection = {
+    'DOWN': 'down',
+    'UP': 'up'
+  };
 
-    function removePhantomFromDOM() {
-        if (mainMenuPhantom.parentElement) {
-            mainMenuPhantom.parentElement.removeChild(mainMenuPhantom);
-        }
-    };
+  function removePhantomFromDOM() {
+    if (mainMenuPhantom.parentElement) {
+      mainMenuPhantom.parentElement.removeChild(mainMenuPhantom);
+    }
+  };
 
-    function addPhantomToDOM() {
-        if (!mainMenuPhantom.parentElement) {
-            mainMenu.parentElement.insertBefore(mainMenuPhantom, mainMenu.nextSibling);
-        }
+  function addPhantomToDOM() {
+    if (!mainMenuPhantom.parentElement) {
+      mainMenu.parentElement.insertBefore(mainMenuPhantom, mainMenu.nextSibling);
+    }
+  }
+
+  function toggleMenuVisibility() {
+    setMenuVisibility(direction === scrollDirection.UP);
+  }
+
+  /**
+   * @param {boolean} isVisible
+   * @param {MouseWheelEvent} event 
+   */
+  function setMenuVisibility(isVisible) {
+    if (isVisible) {
+      mainMenu.classList.add("fixed")
+      addPhantomToDOM();
+    } else {
+      mainMenu.classList.remove("fixed")
+      removePhantomFromDOM();
+    }
+  }
+
+  /**
+   * 
+   * @param {MouseEvent} event 
+   */
+  function determineScrollDirection(event) {
+    if (event.pageY > lastPageY) {
+      direction = scrollDirection.DOWN;
+    } else {
+      direction = scrollDirection.UP;
     }
 
-    function toggleMenuVisibility() {
-        setMenuVisibility(direction === scrollDirection.UP);
+    lastPageY = event.pageY;
+  }
+
+  window.onscroll = function (event) {
+    if (!shouldSetVisibilityFromScroll) {
+      return;
     }
 
-    /**
-     * @param {boolean} isVisible
-     * @param {MouseWheelEvent} event 
-     */
-    function setMenuVisibility(isVisible) {
-        if (isVisible) {
-            mainMenu.classList.add("fixed")
-            addPhantomToDOM();
-        } else {
-            mainMenu.classList.remove("fixed")
-            removePhantomFromDOM();
-        }
-    }
+    window.requestAnimationFrame(function () {
+      determineScrollDirection(event);
+      setMenuVisibility(direction === scrollDirection.UP);
+    });
+  };
 
-    /**
-     * 
-     * @param {MouseEvent} event 
-     */
-    function determineScrollDirection(event) {
-        if (event.pageY > lastPageY) {
-            direction = scrollDirection.DOWN;
-        } else {
-            direction = scrollDirection.UP;
-        }
+  if (window.matchMedia) {
+    const smallScreenQuery = window.matchMedia("(max-width: 1000px)");
 
-        lastPageY = event.pageY;
-    }
+    smallScreenQuery.addListener(function (event) {
+      shouldSetVisibilityFromScroll = event.matches;
+    });
+  }
 
-    window.onscroll = function (event) {
-        if (!shouldSetVisibilityFromScroll) {
-            return;
-        }
+  const toggleButtons = document.getElementsByClassName("toggle-menu-visiblity")
 
-        window.requestAnimationFrame(function () {
-            determineScrollDirection(event);
-            setMenuVisibility(direction === scrollDirection.UP);
-        });
-    };
-
-    if (window.matchMedia) {
-        const smallScreenQuery = window.matchMedia("(max-width: 1000px)");
-
-        smallScreenQuery.addListener(function (event) {
-            shouldSetVisibilityFromScroll = event.matches;
-        });
-    }
-
-    const toggleButtons = document.getElementsByClassName("toggle-menu-visiblity")
-
-    Array.from(toggleButtons).forEach(function (button) {
-        button.onclick = toggleMenuVisibility;
-    })
+  Array.from(toggleButtons).forEach(function (button) {
+    button.onclick = toggleMenuVisibility;
+  })
 })();
 
 (function () {
-    window.addEventListener("load", function () {
-        const links = document.getElementsByClassName("email-me")
+  window.addEventListener("load", function () {
+    const links = document.getElementsByClassName("email-me")
 
-        Array.from(links).forEach(function (element) {
-            const name = element.getAttribute("data-address-name"),
-                domain = element.getAttribute("data-address-domain"),
-                tld = element.getAttribute("data-address-tld");
+    Array.from(links).forEach(function (element) {
+      const name = element.getAttribute("data-address-name"),
+        domain = element.getAttribute("data-address-domain"),
+        tld = element.getAttribute("data-address-tld");
 
-            element.href = "mailto:" + name + "@" + domain + "." + tld;
-        });
+      element.href = "mailto:" + name + "@" + domain + "." + tld;
     });
+  });
 }());
 
 (function () {
-    window.addEventListener("load", function () {
-        const buttons = document.getElementsByClassName("scroll-top")
+  window.addEventListener("load", function () {
+    const buttons = document.getElementsByClassName("scroll-top")
 
-        Array.from(buttons).forEach(function (button) {
-            button.onclick = function () {
-                window.scrollTo({
-                    top: 0,
-                    left: 0,
-                    behavior: "auto"
-                });
-            };
+    Array.from(buttons).forEach(function (button) {
+      button.onclick = function () {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "auto"
         });
+      };
     });
+  });
 }());
